097764031a0dda2e52c3e16898934d05f774ff30
Ludmann Dorian <122395205+DorianLudm@users.noreply.github.com>
2023-10-20 16:32:50 +0200

    Trigger liaison musicale

---
+++ liste_triggers_todo.md
@@ -0,0 +1,9 @@
+# Liste des triggers qu'il faut implémentés  
+## Trigers ajoutés:
+- Vérification de la double existance de la liaison musicale
+
+## Trigers à implémenté:
+- Vérification que le lieu du concert ne soit pas déjà utiliser (vérif heure_début et heure_fin (heure_début + durée) du concert not in between heure_début et heure_fin des concerts qui possède le même lieu)
+- Même vérification qu'au dessus mais sur les disponibilités du groupe
+- Vérification des pré-inscription, ne doit pas dépasser la capacité du lieu
+- Vérification du nombre de billet (COMPLEXE à cause des tickets à plusieurs jours)
--- triggers.sql
+++ triggers.sql
@@ -1,11 +1,19 @@
 delimiter |
-create or replace TRIGGER verif_style_mysical before insert on TABLE for each row
+create or replace TRIGGER verif_liaison_musicale before insert on LIAISONMUSICALE for each row
 begin
-    declare var type;
-    select * into var from ... where ...;
-    if() then
-        set mes = "Texte explicant l'erreur";
+    declare style1 int;
+    declare style2 int;
+    declare mes varchar(255);
+    select idStyle1 into style1 from LIAISONMUSICALE where idStyle1 = new.idStyle2 and idStyle2 = new.idStyle1;
+    select idStyle2 into style2 from LIAISONMUSICALE where idStyle2 = new.idStyle1 and idStyle1 = new.idStyle2;
+    if(style1 != 0 and style2 !=0) then
+        set mes = "La liaison inverse existe déjà";
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
-    end if
-end
-delimiter;
+    end if;
+end |
+delimiter ;
+
+delimiter |
+create or replace TRIGGER verif_capacite_préinscription before insert on PREINSCRIRE for each row
+begin
+    declare 
