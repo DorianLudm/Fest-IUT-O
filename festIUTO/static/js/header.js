eb972ca4880c9261618afe29a25e47cb20d5c39c
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-01-06 17:03:57 +0100

    background header

--- festIUTO/static/js/header.js
+++ festIUTO/static/js/header.js
@@ -5,9 +5,13 @@ window.onscroll = function() {
     if (prevScrollPos > currentScrollPos) {
         // Faire apparaître la barre de navigation lorsque l'utilisateur fait défiler vers le haut
         document.querySelector("nav").style.top = "0";
+        if (currentScrollPos == 0) {
+            document.querySelector("nav").style.backgroundColor = "rgba(255, 255, 255, 0.3)";
+        }
     } else {
         // Cacher la barre de navigation lorsque l'utilisateur fait défiler vers le bas
         document.querySelector("nav").style.top = "-100px"; 
+        document.querySelector("nav").style.backgroundColor = "transparent";
     }
     prevScrollPos = currentScrollPos;
 }
