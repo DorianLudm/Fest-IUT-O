2271f958bdd156678c618038ae768a79afd9a6a8
Ludmann Dorian <dorianludm7@gmail.com>
2024-01-12 11:40:50 +0100

    Changement des données en Json

--- festIUTO/models.py
+++ festIUTO/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from .app import login_manager
 from .connexionPythonSQL import *
+import json
 
 
 
@@ -8,3 +9,8 @@ from .connexionPythonSQL import *
 def load_user(email):
     cnx = get_cnx()
     return get_nom_whith_email(cnx, email)
+
+def get_articles():
+    with open("./static/data/goodies.json") as file:
+        data = json.load(file)
+    return data
